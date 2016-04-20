c3856f09ce119eed5865af840e6fdc0c73696e49
lecorsedegironde <bastien.hoffstetter@free.fr>
2016-04-20 16:16:56 +0900

    Added LibGDX base files

---
+++ android/src/fr/internship2016/prototype/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fr.internship2016.prototype;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fr.internship2016.prototype.PrototypeGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PrototypeGame(), config);
+	}
+}
---
+++ desktop/src/fr/internship2016/prototype/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.internship2016.prototype.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.internship2016.prototype.PrototypeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PrototypeGame(), config);
+	}
+}
