d5fbaef599c3b9b1e4623211ca28b713aad4ad4b
Bastien Hoffstetter <bastien.hoffstetter@free.fr>
2016-05-02 12:48:44 +0900

    Optimised GameScreen, Fixes #9 (#14)

--- core/src/fr/internship2016/prototype/screen/GameScreen.java
+++ core/src/fr/internship2016/prototype/screen/GameScreen.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -30,64 +29,65 @@ import static fr.internship2016.prototype.utils.Constants.*;
  */
 public class GameScreen implements Screen {
 
-    //Camera used to see the world
+    //Camera & viewport
     private OrthographicCamera camera;
-
-    //Renderer to render our shapes
+    private Viewport viewport;
 
     //Draw
     private SpriteBatch batch;
     private ShapeRenderer shapeRenderer;
     private Sprite sprite;
-    private Viewport viewport;
-    private BitmapFont font;
 
+    //Player and reusable vector
     private Player player;
-    private Array<ArmedElement> enemies;
-    private Array<Spell> spells;
+    private Vector2 playerPos;
+
+    //Reusable spell container for adding spells to array
     private Spell addSpell;
 
+    //Arrays of enemies and spells
+    private Array<ArmedElement> enemies;
+    private Array<Spell> spells;
+
     public GameScreen() {
+        //Player position
+        playerPos = new Vector2();
+
+        //Rendering
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
-        font = new BitmapFont();
-
-        font.setColor(Color.GREEN);
-
         sprite = new Sprite(new Texture(Gdx.files.internal("laser.jpg")));
         sprite.setSize(WORLD_WIDTH, WORLD_HEIGHT);
 
+        //Camera
         float aspectRatio = (float) Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth();
-
         camera = new OrthographicCamera();
         viewport = new FillViewport(WORLD_WIDTH * aspectRatio, WORLD_HEIGHT, camera);
         viewport.apply();
-
         camera.position.set(0, WORLD_HEIGHT / 2f, 0);
         camera.update();
 
-        //Player & enemies
+        //Player, enemies & spells
+        enemies = new Array<>();
+        spells = new Array<>();
+
         player = new Player(0.5f, GROUND_HEIGHT, WIDTH_PLAYER, HEIGHT_PLAYER,
                 VELOCITY_X_PLAYER, VELOCITY_Y_PLAYER, true);
-
-        enemies = new Array<>();
 
         Troll troll = new Troll(12f, GROUND_HEIGHT, WIDTH_TROLL, HEIGHT_TROLL,
                 VELOCITY_X_TROLL, VELOCITY_Y_TROLL);
         troll.moveLeft();
-
         enemies.add(troll);
-
-        spells = new Array<>();
     }
 
     @Override
     public void render(float delta) {
-
+        //Clear screen
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        Vector2 playerPos = new Vector2(player.getX(), player.getY());
+        //Update camera pos
+        playerPos.set(player.getX(), player.getY());
         viewport.project(playerPos);
 
         if (playerPos.x > Gdx.graphics.getWidth() * 0.75f) {
@@ -103,29 +103,61 @@ public class GameScreen implements Screen {
 
         camera.update();
 
+        //Player update
         player.update();
         player.setCanStopMovement(true);
 
-        for (ArmedElement e : enemies) {
-            e.update();
-        }
-
+        //Spells update
         for (Spell s : spells) {
+            //Update spell
             s.update(viewport);
+            //Is the spell still active?
+            if (s.isDisappear()) {
+                spells.removeValue(s, true);
+            }
         }
 
+        //TODO: Review
+        //Enemies update
+        for (ArmedElement e : enemies) {
+            //enemies IA
+            EnemiesAI.goToPlayer(e, player);
+            EnemiesAI.enemyReaction(e, player);
+            //Update enemy
+            e.update();
+            //Does it collide with player weapon
+            if (player.isAttacking()) {
+                if (CollisionDetector.isCollision(player.getWeapon(), e) && !player.getWeapon().hasHit()) {
+                    e.hitWeapon();
+                    player.getWeapon().hit();
+                }
+            }
+            //Does it collide with a spell
+            if (spells.size > 0) {
+                for (Spell s : spells) {
+                    if (CollisionDetector.isCollision(s, e)) {
+                        e.hitSpell(s.getDmg());
+                        s.hasHit();
+                    }
+                }
+            }
+            //Is it still alive?
+            if (e.getLife() <= 0) {
+                enemies.removeValue(e, true);
+            }
+
+        }
+
+        //Draw background
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         sprite.draw(batch);
         batch.end();
 
-        batch.begin();
-        font.draw(batch, "Life: " + player.getLife(), 0, 15);
-        font.draw(batch, "Magic: " + player.getMagicPoints(), 0, 15);
-        batch.end();
-
+        //Draw bodies blocks (Filled)
         shapeRenderer.setProjectionMatrix(camera.combined);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        //Player
         if (!player.isInvisible()) {
             if (player.canBeInvisible()) {
                 shapeRenderer.setColor(Color.BLUE);
@@ -141,67 +173,37 @@ public class GameScreen implements Screen {
         }
         //Spells
         for (Spell s : spells) {
-            if (s instanceof FireSpell) shapeRenderer.setColor(Color.MAGENTA);
-            else shapeRenderer.setColor(Color.GOLD);
-
+            if (s instanceof FireSpell){
+                shapeRenderer.setColor(Color.MAGENTA);
+            }
+            else{
+                shapeRenderer.setColor(Color.GOLD);
+            }
             shapeRenderer.rect(s.getX(), s.getY(), s.getW(), s.getH());
         }
-
         shapeRenderer.end();
 
+        //Draw weapons and bodies contours
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        //Player
         shapeRenderer.setColor(Color.GREEN);
         shapeRenderer.rect(player.getX(), player.getY(), player.getW(), player.getH());
+        //Player weapon
         if (player.hasWeapon()) {
             shapeRenderer.setColor(Color.BLACK);
             shapeRenderer.polygon(player.getWeapon().getTransformedVertices());
         }
+        //Enemies weapon
         for (ArmedElement e : enemies) {
             shapeRenderer.setColor(Color.BLACK);
             shapeRenderer.polygon(e.getWeapon().getTransformedVertices());
         }
-
-        //The ground
+        //Ground line
         shapeRenderer.setColor(Color.RED);
         shapeRenderer.line(0, GROUND_HEIGHT, WORLD_WIDTH, GROUND_HEIGHT);
         shapeRenderer.end();
 
-        //TODO: Review
-        for (ArmedElement e : enemies) {
-            if (player.isAttacking()) {
-                if (CollisionDetector.isCollision(player.getWeapon(), e) && !player.getWeapon().hasHit()) {
-                    e.hitWeapon();
-                    player.getWeapon().hit();
-                }
-            }
-            if (spells.size > 0) {
-                for (Spell s : spells) {
-                    if (CollisionDetector.isCollision(s, e)) {
-                        e.hitSpell(s.getDmg());
-                        s.hasHit();
-                    }
-                }
-            }
-        }
-
-        //Are enemies alive?
-        for (ArmedElement e : enemies) {
-            if (e.getLife() <= 0) {
-                enemies.removeValue(e, true);
-            }
-        }
-
-        //Spell disappearing
-        for (Spell s : spells) {
-            if (s.isDisappear()) spells.removeValue(s, true);
-        }
-
-        //enemies IA
-        for (ArmedElement e : enemies) {
-            EnemiesAI.goToPlayer(e, player);
-            EnemiesAI.enemyReaction(e, player);
-        }
-
+        //Inputs events
         if (Gdx.input.isKeyPressed(RIGHT)) {
             player.moveRight();
             player.setCanStopMovement(false);
@@ -226,15 +228,14 @@ public class GameScreen implements Screen {
                 spells.add(addSpell);
             }
         }
+        //Can the player be stopped?
         if (player.canStopMovement()) {
             player.stopMovement();
         }
     }
 
-
     @Override
     public void show() {
-
     }
 
     @Override
@@ -244,17 +245,14 @@ public class GameScreen implements Screen {
 
     @Override
     public void pause() {
-
     }
 
     @Override
     public void resume() {
-
     }
 
     @Override
     public void hide() {
-
     }
 
     @Override
@@ -262,6 +260,5 @@ public class GameScreen implements Screen {
         sprite.getTexture().dispose();
         batch.dispose();
         shapeRenderer.dispose();
-        font.dispose();
     }
 }
